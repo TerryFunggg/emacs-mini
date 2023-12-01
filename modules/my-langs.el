fff758f93a22ba0996d4acc1313b1463c1ac1f8a
TerryFunggg <terryyessfung@gmail.com>
2023-12-01 13:43:32 +0800

    set up eglot server with langs

--- modules/my-langs.el
+++ modules/my-langs.el
@@ -19,4 +19,20 @@
 ;; NOTE: Emacs 29 support tree-sitter, 'M-x treesit-install-language-grammer'
 (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . tsx-ts-mode))
 
+
+;; ===== lsp(eglot) language server config =====
+(add-hook 'web-mode-hook 'eglot-ensure)
+(add-hook 'css-mode-hook 'eglot-ensure)
+(add-hook 'js-mode-hook 'eglot-ensure)
+(add-hook 'tsx-ts-mode-hook 'eglot-ensure)
+(add-hook 'php-mode-hook 'eglot-ensure) ;;npm i intelephense -g
+
+
+(add-to-list 'eglot-server-programs '(php-mode . ("intelephense" "--stdio")))
+(add-to-list 'eglot-server-programs '(web-mode . ("vscode-html-language-server" "--stdio"))
+(add-to-list 'eglot-server-programs '(css-mode . ("vscode-css-language-server" "--stdio")))
+(add-to-list 'eglot-server-programs '(js-mode . ("typescript-language-server" "--stdio")))
+
+
+
 (provide 'my-langs)
