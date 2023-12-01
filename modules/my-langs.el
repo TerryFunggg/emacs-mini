a9618dc8d8e6de3114d6425f0d3587a9d01796f1
TerryFunggg <terryyessfung@gmail.com>
2023-12-01 14:04:10 +0800

    udpate eglot config

--- modules/my-langs.el
+++ modules/my-langs.el
@@ -25,14 +25,12 @@
 (add-hook 'css-mode-hook 'eglot-ensure)
 (add-hook 'js-mode-hook 'eglot-ensure)
 (add-hook 'tsx-ts-mode-hook 'eglot-ensure)
-(add-hook 'php-mode-hook 'eglot-ensure) ;;npm i intelephense -g
-
+(add-hook 'php-mode-hook 'eglot-ensure)
 
 (add-to-list 'eglot-server-programs '(php-mode . ("intelephense" "--stdio")))
 (add-to-list 'eglot-server-programs '(web-mode . ("vscode-html-language-server" "--stdio"))
 (add-to-list 'eglot-server-programs '(css-mode . ("vscode-css-language-server" "--stdio")))
 (add-to-list 'eglot-server-programs '(js-mode . ("typescript-language-server" "--stdio")))
-
-
+(add-to-list 'eglot-server-programs '(tsx-ts-mode . ("typescript-language-server" "--stdio")))
 
 (provide 'my-langs)
