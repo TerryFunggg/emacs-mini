71d7a85ae7df871b3033b9fec3339eec4486cf7e
TerryFunggg <terryyessfung@gmail.com>
2023-12-10 13:49:35 +0800

    add function for revers words and chars

--- modules/my-functions.el
+++ modules/my-functions.el
@@ -1,8 +1,16 @@
-(defun my/reverse-region (beg end)
+(defun my/reverse-region-chars (beg end)
   "Reverse characters on the region"
   (interactive "r")
   (let ((region (buffer-substring beg end)))
     (delete-region beg end)
     (insert (nreverse region))))
+
+
+(defun my/reverse-region-words (beg end)
+  "Reverse words on the region"
+  (interactive "r")
+  (let ((region (buffer-substring beg end)))
+    (delete-region beg end)
+    (insert (string-join (reverse (split-string region "[[:space:]]")) " "))))
 
 (provide 'my-functions)
