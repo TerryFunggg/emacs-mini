3a213e427b2cb973839b024f16208a6be0db765e
TerryFunggg <terryyessfung@gmail.com>
2024-05-16 22:41:17 +0800

    update language config

--- modules/my-langs.el
+++ modules/my-langs.el
@@ -1,16 +1,26 @@
-(use-package web-mode)
-(add-hook 'web-mode-hook 'lsp-mode)
+(use-package web-mode
+  :config
+  (setq web-mode-enable-auto-pairing t
+        web-mode-enable-css-colorization t
+        web-mode-enable-heredoc-fontification t
+        web-mode-enable-current-element-highlight t
+        web-mode-enable-current-column-highlight t)
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)))
+
+(add-hook 'web-mode-hook 'lsp)
 
 (use-package emmet-mode
   :hook (web-mode tsx-ts-mode)
   :config
   (add-hook 'emmet-mode-hook (lambda ()
                                (setq emmet-indent-after-insert t))))
+(add-hook 'web-mode-hook 'emmet-mode)
 
 (use-package php-mode
-  :mode "\\.php\\'"
-  )
-(add-hook 'web-mode-hook 'lsp-mode)
+  :mode "\\.php\\'")
+(add-hook 'php-mode-hook 'lsp)
 
 
 ;; ruby
@@ -19,13 +29,13 @@
 (use-package inf-ruby)
 (add-hook 'ruby-ts-mode-hook 'inf-ruby-minor-mode)
 (add-hook 'ruby-ts-mode-hook 'highlight-indent-guides-mode)
-(add-hook 'ruby-mode-hook 'ls-pmode)
+(add-hook 'ruby-mode-hook 'lsp)
 
 ;; python
 (use-package highlight-indent-guides)
 ;;dd-hook 'python-mode-hook 'highlight-indent-guides-mode)
 (add-hook 'python-ts-mode-hook 'highlight-indent-guides-mode)
-(add-hook 'python-mode-hook 'lsp-mode)
+(add-hook 'python-mode-hook 'lsp)
 
 ;; golang
 (use-package go-mode)
