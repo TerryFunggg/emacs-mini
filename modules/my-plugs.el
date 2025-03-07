310583f0ba4b102964f0bb790145de0a414d5a6a
TerryFunggg <terryyessfung@gmail.com>
2025-03-07 23:06:06 +0800

    fix tab conflict

--- modules/my-plugs.el
+++ modules/my-plugs.el
@@ -2,8 +2,6 @@
 (global-flycheck-mode)
 
 (package-install 'company)
-
-
 (setq company-minimum-prefix-length 2
       company-idle-delay 0.1
       company-selection-wrap-around t ;; cycle selection
@@ -11,8 +9,7 @@
       company-tooltip-limit 6
       company-tooltip-flip-when-above t
       company-global-modes '(not message-mode eshell-mode))
-(global-set-key (kbd "<tab>") #'company-indent-or-complete-common)
-
+;;(global-set-key (kbd "<tab>") #'company-indent-or-complete-common)
 (global-company-mode)
 
 (provide 'my-plugs)
